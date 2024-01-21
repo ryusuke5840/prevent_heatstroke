ed01ef32719851e3230bb6d61f14393d6fec0394
Morita <sryusuke1030@gmail.com>
2024-01-22 02:52:34 +0900

    日本語化の指定

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Apps
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.il8n.default = :ja
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
