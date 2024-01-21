d35e3c17b4c6e9fb42f5ebcbdb0b7ae516acaf21
Morita <sryusuke1030@gmail.com>
2024-01-22 02:29:44 +0900

    migrationファイルの編集

--- db/migrate/20240121172821_create_rooms.rb
+++ db/migrate/20240121172821_create_rooms.rb
@@ -1,11 +1,11 @@
 class CreateRooms < ActiveRecord::Migration[7.0]
   def change
     create_table :rooms do |t|
-      t.string :name
-      t.string :serialnumber
-      t.string :user_email
-      t.string :family_email
-      t.integer :allert_count
+      t.string :name, null: false
+      t.string :serialnumber, null: false
+      t.string :user_email, null: false
+      t.string :family_email, null: false
+      t.integer :allert_count, null: false , default: 0
 
       t.timestamps
     end
