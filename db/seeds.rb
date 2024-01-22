8cfe3468fdf44c6b59cff4fea221e5ba66a26c8c
Morita <sryusuke1030@gmail.com>
2024-01-22 15:44:13 +0900

    seedsファイルの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "csv"
+
+Room.create(
+  id: 1,
+  name: 'My room',
+  serialnumber: '106438091648560',
+  user_email: 'send.s2027091@gmail.com',
+  family_email: 's2027091@nbu.ac.jp',
+)
+Room.create(
+  id: 2,
+  name: 'Living',
+  serialnumber: '229334319410736',
+  user_email: 'send.s2027091@gmail.com',
+  family_email: 's2027091@nbu.ac.jp',
+)
+Room.create(
+  id: 3,
+  name: 'Test room',
+  serialnumber: '202027091',
+  user_email: 'sryusuke1030@gmail.com',
+  family_email: 's2027091@nbu.ac.jp',
+)
+
+CSV.foreach('db/temperature.csv', headers: true) do |row|
+  Temperature.create(
+    temperature: row['temperature'],
+    room_id: row['room_id'],
+    created_at: row['created_at']
+  )
+end
